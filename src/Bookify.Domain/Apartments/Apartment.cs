c9346ca928ce5dc39342dd3cf7889cf4f8e4aa90
Felipe Santos <rfelipe273@gmail.com>
2023-08-13 16:23:31 -0300

    Add Apartment value objects

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -9,22 +9,23 @@ namespace Bookify.Domain.Apartments
 {
     public class Apartment : Entity
     {
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public string Country { get; private set; }
-        public string State { get; private set; }
-        public string ZipCode { get; private set; }
-        public string City { get; private set; }
-        public string Street { get; private set; }
-        public decimal PriceAmount { get; private set; }
-        public decimal PriceCurrency { get; private set; }
-        public decimal CleaningFeeAmount { get; private set; }
-        public decimal CleaningFeeCurrency { get; private set; }
+        public Name Name { get; private set; }
+        public Description Description { get; private set; }
+        public Address Address { get; private set; }
+        public Money Price { get; private set; }
+        public Money CleaningFee { get; private set; }
         public DateTime? LastBookedOnUtc { get; private set; }
         public List<Amenity> Amenities { get; private set; }
 
-        public Apartment(Guid id) : base(id)
+        public Apartment(Guid id, Name name, Description description, Address address, Money price, Money cleaningFee, DateTime? lastBookedOnUtc, List<Amenity> amenities) : base(id)
         {
+            Name = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            CleaningFee = cleaningFee;
+            LastBookedOnUtc = lastBookedOnUtc;
+            Amenities = amenities;
         }
     }
 }
