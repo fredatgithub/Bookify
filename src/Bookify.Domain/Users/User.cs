4a484aa423065e66151625be7a344099e8076370
Felipe Santos <rfelipe273@gmail.com>
2023-08-13 22:06:45 -0300

    Add Mediatr Add domain events and user created domain event

--- src/Bookify.Domain/Users/User.cs
+++ src/Bookify.Domain/Users/User.cs
@@ -1,4 +1,6 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
+
 namespace Bookify.Domain.Users
 {
     public sealed class User : Entity
@@ -16,6 +18,7 @@ namespace Bookify.Domain.Users
         public static User Create(FirstName firstName, LastName lastName, Email email)
         {
             var user = new User(Guid.NewGuid(), firstName, lastName, email);
+            user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
             return user;
         }
     }
