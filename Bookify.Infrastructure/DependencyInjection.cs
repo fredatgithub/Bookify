016d4e036fc7a0053741821c34d6c826b81948ba
Felipe Santos <rfelipe273@gmail.com>
2023-11-17 21:58:26 -0300

    Infra

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
+using Bookify.Domain.Users;
+using Bookify.Infrastructure.Repositories;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Abstractions;
+using Bookify.Application.Abstractions.Data;
+using Bookify.Infrastructure.Data;
+using Dapper;
+
+namespace Bookify.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString =
+            configuration.GetConnectionString("Database") ??
+            throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IApartmentRepository, ApartmentRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+
+        return services;
+    }
+}
+
